e0943fc9ab342ebe2b7ff5cc02cc15fd8728bfd4
nakamuuu-prog <affilimoney2@gmail.com>
2025-06-19 09:01:42 +0900

    invoice.performancesのfor文を分割

--- 1/index.js
+++ 1/index.js
@@ -1,17 +1,19 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    // ボリューム特典のポイントを加算
-    volumeCredits += volumeCreditsFor(perf);
-
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
+  }
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    // ボリューム特典のポイントを加算
+    volumeCredits += volumeCreditsFor(perf);
   }
 
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
