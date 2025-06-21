fb6d2baf546d71c451697af3edbc85db0545d0c8
nakamuuu-prog <affilimoney2@gmail.com>
2025-06-21 16:00:53 +0900

    amountFor関数をPerformanceCalculatorに移動

--- 1/createStatementData.js
+++ 1/createStatementData.js
@@ -13,37 +13,13 @@ export default function createStatementData(invoice, plays) {
     );
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformances) {
-    let result = 0;
-
-    switch (aPerformances.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformances.audience > 30) {
-          result += 1000 * (aPerformances.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformances.audience > 20) {
-          result += 10000 + 500 * (aPerformances.audience - 20);
-        }
-        result += 300 * aPerformances.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(aPerformances).type}`);
-    }
-
-    return result;
   }
 
   function volumeCreditsFor(aPerformance) {
@@ -70,5 +46,29 @@ class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+
+    return result;
   }
 }
