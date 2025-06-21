7f34394baf8e0697ae215e60f066391cc4f716b1
nakamuuu-prog <affilimoney2@gmail.com>
2025-06-21 16:43:02 +0900

    volumeCreditsFor関数をPerformanceCalculatorに移動

--- 1/createStatementData.js
+++ 1/createStatementData.js
@@ -14,23 +14,12 @@ export default function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // ボリューム特典のポイントを加算
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは 10 人につき、さらにポイントを加算
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
   }
 
   function totalAmount(data) {
@@ -68,6 +57,17 @@ class PerformanceCalculator {
       default:
         throw new Error(`unknown type: ${this.play.type}`);
     }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    // ボリューム特典のポイントを加算
+    result += Math.max(this.performance.audience - 30, 0);
+    // 喜劇のときは 10 人につき、さらにポイントを加算
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
 
     return result;
   }
