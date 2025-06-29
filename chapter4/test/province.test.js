10aa176afdcb167e758cc49b1d7fd52822210230
nakamuuu-prog <affilimoney2@gmail.com>
2025-06-29 17:37:10 +0900

    境界値のテストを実装

--- chapter4/test/province.test.js
+++ chapter4/test/province.test.js
@@ -17,6 +17,40 @@ describe('province', function () {
     expect(asia.shortfall).equal(-6);
     expect(asia.profit).equal(292);
   });
+  it('zoro demand', function () {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+  it('negative demand', function () {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+  it('empty string demand', function () {
+    asia.demand = '';
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
+});
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: 'No Producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function () {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it('profit', function () {
+    expect(noProducers.profit).equal(0);
+  });
 });
 
 function sampleProvinceData() {
