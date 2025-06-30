b7f79e39c6123cea2243179c4316c0ed9dd0f7fa
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-01 08:03:21 +0900

    quantityとitemPriceの計算を変数化

--- chapter6/変数の抽出/index.js
+++ chapter6/変数の抽出/index.js
@@ -1,8 +1,9 @@
 function price(order) {
   // price = base price - quantity discount + shipping
+  const basePrice = order.quantity * order.itemPrice;
   return (
-    order.quantity * order.itemPrice -
+    basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+    Math.min(basePrice * 0.1, 100)
   );
 }
