e05385aaeb4d911743381be3614b7b34bc209f23
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-01 07:47:59 +0900

    入り組んだケースのリファクタリング

--- chapter6/関数のインライン化/index.js
+++ chapter6/関数のインライン化/index.js
@@ -1,3 +1,16 @@
+// 単純なケース
 function rating(aDriver) {
   return aDriver.numberOfLateDeliveries.length > 5 ? 2 : 1;
+}
+
+// 入り組んだケース
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(['name', aCustomer.name]);
+  out.push(['location', aCustomer.location]);
 }
