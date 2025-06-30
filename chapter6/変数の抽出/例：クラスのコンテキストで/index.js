352bcedff204d2161665844ba49916d462d704d4
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-01 08:11:05 +0900

    quantityとitemPriceの計算をgetterに移動

--- chapter6/変数の抽出/例：クラスのコンテキストで/index.js
+++ chapter6/変数の抽出/例：クラスのコンテキストで/index.js
@@ -13,9 +13,13 @@ class Order {
 
   get price() {
     return (
-      this.quantity * this.itemPrice -
+      this.basePrice -
       Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+      Math.min(this.basePrice * 0.1, 100)
     );
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
   }
 }
