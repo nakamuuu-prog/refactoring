d2f2d1dc8727dd79f7043e8da5c30228c874f7d6
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-01 08:12:14 +0900

    数量値引きをgetterに移動

--- chapter6/変数の抽出/例：クラスのコンテキストで/index.js
+++ chapter6/変数の抽出/例：クラスのコンテキストで/index.js
@@ -14,12 +14,16 @@ class Order {
   get price() {
     return (
       this.basePrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      this.quantityDiscount +
       Math.min(this.basePrice * 0.1, 100)
     );
   }
 
   get basePrice() {
     return this.quantity * this.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
   }
 }
