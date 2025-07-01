b36c20a6a2b617dad608445299341584b69cd730
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-02 08:42:25 +0900

    関数の抽出

--- chapter6/関数宣言の変更/index.js
+++ chapter6/関数宣言の変更/index.js
@@ -2,5 +2,9 @@ const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
 
 function inNewEngland(sCustomer) {
   const stateCode = sCustomer.address.state;
+  return xxNewinEngland(stateCode);
+}
+
+function xxNewinEngland(stateCode) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
