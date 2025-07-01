5fdd99a33cef2e914994553c40033c77f022f8b6
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-02 08:41:11 +0900

    sCustomer.address.stateを変数に抽出

--- chapter6/関数宣言の変更/index.js
+++ chapter6/関数宣言の変更/index.js
@@ -1,5 +1,6 @@
 const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
 
 function inNewEngland(sCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(sCustomer.address.state);
+  const stateCode = sCustomer.address.state;
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
