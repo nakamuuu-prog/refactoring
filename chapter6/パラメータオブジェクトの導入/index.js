74abcabd5bd5188c3feb8ec0527c6679cae31adc
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-05 16:48:52 +0900

    NumberRangeクラスを用意

--- chapter6/パラメータオブジェクトの導入/index.js
+++ chapter6/パラメータオブジェクトの導入/index.js
@@ -18,3 +18,16 @@ alerts = readingsOutsideRange(
   operatingPlan.temperatureFloor,
   operatingPlan.temperatureCeiling
 );
+
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min: min, max: max };
+  }
+
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+}
