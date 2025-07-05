1a83cf5f37d1e79fdd905d53ca5093ff3d5db0db
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-05 21:05:48 +0900

    課税対象額の計算を関数に抽出

--- chapter6/関数群のクラスへの集約/index.js
+++ chapter6/関数群のクラスへの集約/index.js
@@ -7,10 +7,10 @@ function client1() {
 function client2() {
   const rawReading = acquireReading();
   const aReading = new Reading(rawReading);
-  const taxableCharge = Math.max(
-    0,
-    aReading.baseCharge - taxThreshold(aReading.year)
-  );
+  const taxableCharge = taxableChargeFn(aReading);
+  function taxableChargeFn(aReading) {
+    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+  }
 }
 
 function client3() {
