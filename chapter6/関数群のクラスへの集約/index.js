0050e8aac1d1185dd79102c63456e0def5b68eb2
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-05 20:55:46 +0900

    Readingクラスを実装

--- chapter6/関数群のクラスへの集約/index.js
+++ chapter6/関数群のクラスへの集約/index.js
@@ -17,3 +17,24 @@ function client3() {
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
   }
 }
+
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+}
