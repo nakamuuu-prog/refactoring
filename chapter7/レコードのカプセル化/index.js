fafc39373a419a5b5666521bf8c701c714103878
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-07 08:48:34 +0900

    Organizationクラスを実装

--- chapter7/レコードのカプセル化/index.js
+++ chapter7/レコードのカプセル化/index.js
@@ -1,7 +1,19 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
 
 function getRawDataOfOrganization() {
+  return organization._data;
+}
+function getOrganization() {
   return organization;
+}
+
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
 }
 
 // 参照時
