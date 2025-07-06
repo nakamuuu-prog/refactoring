ed0305f21f08e28b2308f6909bd2426bccecb078
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-06 10:47:12 +0900

    discountを中間データ構造で渡す

--- chapter6/フェーズの分離/index.js
+++ chapter6/フェーズの分離/index.js
@@ -8,16 +8,17 @@ function priceOrder(product, quantity, shippingMethod) {
   const priceData = {
     basePrice: basePrice,
     quantity: quantity,
+    discount: discount,
   };
-  const price = applyShipping(priceData, shippingMethod, discount);
+  const price = applyShipping(priceData, shippingMethod);
   return price;
 }
-function applyShipping(priceData, shippingMethod, discount) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase =
     priceData.basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - discount + shippingCost;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
   return price;
 }
