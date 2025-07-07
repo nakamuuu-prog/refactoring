3820eadde750818fd4862631c4c6330e67fa5605
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-08 08:53:26 +0900

    読み取り用の関数をクラスに実装

--- chapter7/レコードのカプセル化/入れ子レコードのカプセル化/index.js
+++ chapter7/レコードのカプセル化/入れ子レコードのカプセル化/index.js
@@ -22,6 +22,10 @@ class CustomerData {
     return _.cloneDeep(this._data);
   }
 
+  usage(customerID, year, month) {
+    return this._data[customerID].usages[year][month];
+  }
+
   setUsage(customerID, year, month, amount) {
     this._data[customerID].usages[year][month] = amount;
   }
@@ -51,8 +55,7 @@ getCustomerData.setUsage(customerID, year, month, amount);
 
 // sample read...
 function compareUsage(customerID, laterYear, month) {
-  const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
-  const earlier =
-    getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
+  const later = getCustomerData().usage(customerID, laterYear, month);
+  const earlier = getCustomerData().usage(customerID, laterYear - 1, month);
   return { laterAmount: later, change: later - earlier }; //前年同月比較
 }
