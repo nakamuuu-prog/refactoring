c1f144ba6f819716d8574ca16e418e64c499262c
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-10 08:05:00 +0900

    Priorityクラスを実装し、OrderのゲッターセッターでPriorityクラスを利用するようにする

--- chapter7/オブジェクトによるプリミティブの置き換え/index.js
+++ chapter7/オブジェクトによるプリミティブの置き換え/index.js
@@ -1,12 +1,21 @@
+class Priority {
+  constructor(value) {
+    this._value = value;
+  }
+  toString() {
+    return this._value;
+  }
+}
+
 class Order {
   constructor(data) {
     this.priority = data.priority;
   }
   get priority() {
-    return this._priority;
+    return this._priority.toString();
   }
   set priority(aString) {
-    this._priority = aString;
+    this._priority = new Priority(aString);
   }
 }
 
