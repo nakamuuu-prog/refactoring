6495b83817e580d77581ac068c1bf6698652cb9b
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-11 08:55:42 +0900

    不要になったTrackingInformationクラスを削除

--- chapter7/クラスのインライン化/index.js
+++ chapter7/クラスのインライン化/index.js
@@ -1,25 +1,5 @@
 aShipment.shippingCompany = request.vendor;
 
-class TrackingInformation {
-  get shippingCompany() {
-    return this._shippingCompany;
-  }
-  set shippingCompany(arg) {
-    this._shippingCompany = arg;
-  }
-
-  get trackingNumber() {
-    return this._trackingNumber;
-  }
-  set trackingNumber(arg) {
-    this._trackingNumber = arg;
-  }
-
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
-}
-
 class Shipment {
   get trackingInfo() {
     return `${this.shippingCompany}: ${this.trackingNumber}`;
