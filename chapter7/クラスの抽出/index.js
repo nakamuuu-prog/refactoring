14b1a62fd31ea27ad30be6f8cfd64b095d669f17
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-11 08:29:04 +0900

    officeNumberフィールドを移動

--- chapter7/クラスの抽出/index.js
+++ chapter7/クラスの抽出/index.js
@@ -22,10 +22,10 @@ class Person {
   }
 
   get officeNumber() {
-    return this._officeNumber;
+    return this._telephoneNumber.officeNumber;
   }
   set officeNumber(arg) {
-    this._officeNumber = arg;
+    this._telephoneNumber.officeNumber = arg;
   }
 }
 
@@ -35,5 +35,12 @@ class TelephoneNumber {
   }
   set officeAreaCode(arg) {
     this._officeAreaCode = arg;
+  }
+
+  get officeNumber() {
+    return this._officeNumber;
+  }
+  set officeNumber(arg) {
+    this._officeNumber = arg;
   }
 }
