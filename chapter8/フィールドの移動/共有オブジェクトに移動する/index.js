655714b653d9aa06d0d10f76089c56b75c876caa
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-15 13:33:33 +0900

    AccountTypeにinterestRateフィールドとアクセサを実装

--- chapter8/フィールドの移動/共有オブジェクトに移動する/index.js
+++ chapter8/フィールドの移動/共有オブジェクトに移動する/index.js
@@ -11,7 +11,11 @@ class Account {
 }
 
 class AccountType {
-  constructor(nameString) {
+  constructor(nameString, interestRate) {
     this._name = nameString;
+    this._interestRate = interestRate;
+  }
+  get interestRate() {
+    return this._interestRate;
   }
 }
