e7176f6335c094b094d8e839702f5037aee06999
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-15 13:40:56 +0900

    AccountTypeからinterestRateを取得する

--- chapter8/フィールドの移動/共有オブジェクトに移動する/index.js
+++ chapter8/フィールドの移動/共有オブジェクトに移動する/index.js
@@ -1,13 +1,12 @@
+// アサーションが通り続けたら、安全にフィールドを移動できたことになるので、不要になるコードを削除する
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    assert(interestRate === this._type.interestRate);
-    this._interestRate = interestRate;
   }
 
   get interestRate() {
-    return this._interestRate;
+    return this._type.interestRate;
   }
 }
 
