839f2a69366fd118a9040102abe4ae1b5e46fe3e
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-18 08:23:22 +0900

    変数の分離

--- chapter8/index.js
+++ chapter8/index.js
@@ -1,13 +1,15 @@
-function acquireData(input) {
-  const lines = input.split('\n');
-  const result = lines
-    .slice(1)
-    .filter((line) => line.trim() !== '')
-    .map((line) => line.split(','))
-    .filter((record) => record[1].trim() === 'India')
-    .map((record) => ({
-      city: record[0].trim(),
-      phone: record[2].trim(),
-    }));
+function distanceTravelled(scenario, time) {
+  let result;
+  let acc = scenario.primaryForce / scenario.mass;
+  let primaryTime = Math.min(time, scenario.delay);
+  result = 0.5 * acc * primaryTime * primaryTime;
+  let secondaryTime = time - scenario.delay;
+  if (secondaryTime > 0) {
+    let primaryVelocity = acc * scenario.delay;
+    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    result +=
+      primaryVelocity * secondaryTime +
+      0.5 * acc * secondaryTime * secondaryTime;
+  }
   return result;
 }
