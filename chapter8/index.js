a073c308b6fca15ed3c8cd0f8d0bd1d165340607
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-18 08:26:41 +0900

    if内のacc変数の命名を変更しconstに修正

--- chapter8/index.js
+++ chapter8/index.js
@@ -3,14 +3,14 @@ function distanceTravelled(scenario, time) {
   const primaryAcceleration = scenario.primaryForce / scenario.mass;
   let primaryTime = Math.min(time, scenario.delay);
   result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
-
   let secondaryTime = time - scenario.delay;
   if (secondaryTime > 0) {
     let primaryVelocity = primaryAcceleration * scenario.delay;
-    let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    const secondaryAcceleration =
+      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
     result +=
       primaryVelocity * secondaryTime +
-      0.5 * acc * secondaryTime * secondaryTime;
+      0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
   }
   return result;
 }
