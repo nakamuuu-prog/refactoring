bde87794b71dfb7245e8ff3dc568c2187aa245c5
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-19 12:44:06 +0900

    ファクトリーを実装

--- chapter8/index.js
+++ chapter8/index.js
@@ -7,12 +7,31 @@ function speeds(birds) {
 }
 
 function plumage(bird) {
-  return new Bird(bird).plumage;
+  return createBird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {
-  return new Bird(bird).airSpeedVelocity;
+  return createBird(bird).airSpeedVelocity;
 }
+
+function createBird(bird) {
+  switch (bird.type) {
+    case 'EuropeanSwallow':
+      return new EuropeanSwallow(bird);
+    case 'AfricanSwallow':
+      return new AfricanSwallow(bird);
+    case 'NorwegianBlueParrot':
+      return new NorwegianBlueParrot(bird);
+    default:
+      return new Bird(bird);
+  }
+}
+
+class EuropeanSwallow extends Bird {}
+
+class AfricanSwallow extends Bird {}
+
+class NorwegianBlueParrot extends Bird {}
 
 class Bird {
   constructor(birdObject) {
