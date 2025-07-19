42a1de4b386425331e81af3512edbaea165554d0
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-19 12:57:11 +0900

    バリエーションに対して森もーFISMを適用

--- chapter8/index.js
+++ chapter8/index.js
@@ -1,69 +1,43 @@
-function plumages(birds) {
-  return new Map(birds.map((b) => [b.name, plumage(b)]));
+function rating(voyage, history) {
+  const vpf = voyageProfitFactor(voyage, history);
+  const vr = voyageRisk(voyage);
+  const chr = captainHistoryRisk(voyage, history);
+  if (vpf * 3 > vr + chr * 2) return 'A';
+  else return 'B';
 }
 
-function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+function voyageRisk(voyage) {
+  let result = 1;
+  if (voyage.length > 4) result += 2;
+  if (voyage.length > 8) result += voyage.length - 8;
+  if (['china', 'east-indies'].includes(voyage.zone)) result += 4;
+  return Math.max(result, 0);
 }
 
-function plumage(bird) {
-  return createBird(bird).plumage;
+function captainHistoryRisk(voyage, history) {
+  let result = 1;
+  if (history.length < 5) result += 4;
+  result += history.filter((v) => v.profit < 0).length;
+  if (voyage.zone === 'china' && hasChina(history)) result -= 2;
+  return Math.max(result, 0);
 }
 
-function airSpeedVelocity(bird) {
-  return createBird(bird).airSpeedVelocity;
+function hasChina(history) {
+  return history.some((v) => 'china' === v.zone);
 }
 
-function createBird(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return new EuropeanSwallow(bird);
-    case 'AfricanSwallow':
-      return new AfricanSwallow(bird);
-    case 'NorwegianBlueParrot':
-      return new NorwegianBlueParrot(bird);
-    default:
-      return new Bird(bird);
+function voyageProfitFactor(voyage, history) {
+  let result = 2;
+  if (voyage.zone === 'china') result += 1;
+  if (voyage.zone === 'east-indies') result += 1;
+  if (voyage.zone === 'china' && hasChina(history)) {
+    result += 3;
+    if (history.length > 10) result += 1;
+    if (voyage.length > 12) result += 1;
+    if (voyage.length > 18) result -= 1;
+  } else {
+    if (history.length > 8) result += 1;
+    if (voyage.length > 14) result -= 1;
   }
-}
-
-class EuropeanSwallow {
-  get plumage() {
-    return 'average';
-  }
-  get airSpeedVelocity() {
-    return 35;
-  }
-}
-
-class AfricanSwallow extends Bird {
-  get plumage() {
-    return this.numberOfCoconuts > 2 ? 'tired' : 'average';
-  }
-  get airSpeedVelocity() {
-    return 40 - 2 * this.numberOfCoconuts;
-  }
-}
-
-class NorwegianBlueParrot extends Bird {
-  get plumage() {
-    return this.voltage > 100 ? 'scorched' : 'beautiful';
-  }
-  get airSpeedVelocity() {
-    return this.isNailed ? 0 : 10 + this.voltage / 10;
-  }
-}
-
-class Bird {
-  constructor(birdObject) {
-    Object.assign(this, birdObject);
-  }
-
-  get plumage() {
-    return 'unknown';
-  }
-
-  get airSpeedVelocity() {
-    return null;
-  }
+  return result;
 }
