6b3347f4d7a3842dcaf433c9bd5ceca471472309
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-19 12:53:34 +0900

    airSpeedVelocityも同じようにオーバーライド

--- chapter8/index.js
+++ chapter8/index.js
@@ -31,17 +31,26 @@ class EuropeanSwallow {
   get plumage() {
     return 'average';
   }
+  get airSpeedVelocity() {
+    return 35;
+  }
 }
 
 class AfricanSwallow extends Bird {
   get plumage() {
     return this.numberOfCoconuts > 2 ? 'tired' : 'average';
   }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
 }
 
 class NorwegianBlueParrot extends Bird {
   get plumage() {
     return this.voltage > 100 ? 'scorched' : 'beautiful';
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
   }
 }
 
@@ -55,15 +64,6 @@ class Bird {
   }
 
   get airSpeedVelocity() {
-    switch (this.type) {
-      case 'EuropeanSwallow':
-        return 35;
-      case 'AfricanSwallow':
-        return 40 - 2 * this.numberOfCoconuts;
-      case 'NorwegianBlueParrot':
-        return this.isNailed ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
