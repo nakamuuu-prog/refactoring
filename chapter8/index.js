1972d6c0b4f132bda70b5ff910af7da9e98421fc
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-26 09:26:06 +0900

    あ

--- chapter8/index.js
+++ chapter8/index.js
@@ -1,19 +1,25 @@
 function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel -= 10;
-    highMedicalRiskFlag = true;
-  }
+class Scorer {
+  execute(candidate, medicalExam, scoringGuide) {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
 
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
+    if (medicalExam.isSmoker) {
+      healthLevel -= 10;
+      highMedicalRiskFlag = true;
+    }
+
+    let certificationGrade = 'regular';
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // このようなコードがずっと続く
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  // このようなコードがずっと続く
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
