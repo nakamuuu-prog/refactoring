6bb54dc3dd9d12140a1bc53ccdede9e791d93dfd
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-26 09:36:08 +0900

    あ

--- chapter8/index.js
+++ chapter8/index.js
@@ -1,20 +1,23 @@
 function score(candidate, medicalExam, scoringGuide) {
-  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+  return new Scorer(candidate).execute(medicalExam, scoringGuide);
 }
 
 class Scorer {
-  execute(candidate, medicalExam, scoringGuide) {
+  constructor(candidate) {
+    this._candidate = candidate;
+  }
+
+  execute(medicalExam, scoringGuide) {
     let result = 0;
     let healthLevel = 0;
     let highMedicalRiskFlag = false;
 
     if (medicalExam.isSmoker) {
-      healthLevel -= 10;
+      healthLevel += 10;
       highMedicalRiskFlag = true;
     }
-
     let certificationGrade = 'regular';
-    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+    if (scoringGuide.stateWithLowCertification(this._candidate.originState)) {
       certificationGrade = 'low';
       result -= 5;
     }
