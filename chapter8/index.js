ceed0240ff58ab9a896065818ea76ce858e691f0
nakamuuu-prog <affilimoney2@gmail.com>
2025-07-26 09:43:34 +0900

    あ

--- chapter8/index.js
+++ chapter8/index.js
@@ -14,10 +14,7 @@ class Scorer {
     this._healthLevel = 0;
     this._highMedicalRiskFlag = false;
 
-    if (this._medicalExam.isSmoker) {
-      this._healthLevel += 10;
-      this._highMedicalRiskFlag = true;
-    }
+    this.scoreSmoking();
 
     this._certificationGrade = 'regular';
     if (
@@ -30,5 +27,12 @@ class Scorer {
     // このようなコードがずっと続く
     this._result -= Math.max(this._healthLevel - 5, 0);
     return this._result;
+  }
+
+  scoreSmoking() {
+    if (this._medicalExam.isSmoker) {
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
+    }
   }
 }
